b9a9573cec6f7e7c6544b307eec411ed5283bad6
SteGlaset <90640042+SteGlaset@users.noreply.github.com>
2023-09-11 12:30:58 +0300

    chore: add scss and google fonts to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: {enabled: true},
+    modules: ['@nuxtjs/google-fonts'],
+    googleFonts: {
+        families: {
+            Roboto: true,
+        }
+    },
+    css: [
+        '@/styles/normalize.css',
+        '@/styles/main.scss'
+    ],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@use "@/styles/_variables.scss" as *;'
+                }
+            }
+        }
+    }
 })
